c7ff4f142c3104eee7266f8e459348440e759d74
leo-guinan <leo@copy.ai>
2023-04-08 19:05:46 -0400

    removing nltk from build script because it isn't currently used
    Former-commit-id: c8edc07e96e034df0a299bbc6c99b996de2fcf9c

--- build.sh
+++ build.sh
@@ -11,5 +11,3 @@ poetry run pip install --force-reinstall -U setuptools
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python -m nltk.downloader -d /opt/render/project/src/nltk all
