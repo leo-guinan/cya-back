14fb14f0c1550d2a34c0b42c02ab7b496d0c1ed8
leo-guinan <leo@copy.ai>
2023-04-08 18:24:18 -0400

    changing to use openai embeddings function
    Former-commit-id: 807353466ff5db9dcbb56835ac7247b9c694180c

--- embeddings/vectorstore.py
+++ embeddings/vectorstore.py
@@ -1,6 +1,7 @@
 import chromadb
 from chromadb.config import Settings
 from decouple import config
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
 
@@ -8,6 +9,7 @@ class Vectorstore:
     collections = {}
 
     def __init__(self):
+        self.embeddings = OpenAIEmbeddings(openai_api_key=config('OPENAI_API_KEY'))
         self.settings = Settings(chroma_api_impl="rest",
                                  chroma_server_host=config('CHROMA_SERVER_HOST'),
                                  chroma_server_http_port=8000)
@@ -16,11 +18,13 @@ class Vectorstore:
         for collection in collections:
             if collection.name not in self.collections:
                 self.collections[collection.name] = Chroma(
+                    embedding_function=self.embeddings,
                     collection_name=collection.name,
                     client_settings=self.settings)
 
     def create_collection(self, name):
         collection = Chroma(
+            embedding_function=self.embeddings,
             collection_name=name,
             client_settings=self.settings)
         self.collections[name] = collection
