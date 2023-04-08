0fc2c55ddf5065bc97df0326352d693693c1bdc2
leo-guinan <leo@copy.ai>
2023-04-08 16:22:42 -0400

    name parameter keyword
    Former-commit-id: e2331bcaebbe1bf833cd210107fec16b971b2a46

--- spark/tasks.py
+++ spark/tasks.py
@@ -3,7 +3,7 @@ from embeddings.documents import save_document
 from spark.manage import add_rss_feed
 
 
-@app.task("spark.process_rss_feed")
+@app.task(name="spark.process_rss_feed")
 def process_rss_feed(title, url):
     link = add_rss_feed(url, title)
 
