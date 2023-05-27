9dd5f248951723b2dbfd6a8ffbdb71c4de68beea
leo-guinan <leo@copy.ai>
2023-05-27 14:21:48 -0400

    initial infrastructure for chat widget
    Former-commit-id: eaba6fb739c0b13f36fecc414bcb2fa09d54adfd

---
+++ chat/service.py
@@ -0,0 +1,52 @@
+import logging
+from typing import List
+
+from decouple import config
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import BaseMessage, HumanMessage, BaseChatMessageHistory
+
+from embeddings.vectorstore import Vectorstore
+
+logger = logging.getLogger(__name__)
+
+class PassedInChatHistory(BaseChatMessageHistory):
+    _messages = []
+
+    @property
+    def messages(self) -> List[BaseMessage]:
+        return self._messages
+
+    def add_user_message(self, message) -> None:
+        if type(message) == dict:
+            message = message['message']
+        self._messages.append(HumanMessage(content=message))
+
+    def add_ai_message(self, message) -> None:
+        if type(message) == dict:
+            message = message['message']
+        self._messages.append(HumanMessage(content=message))
+
+    def clear(self) -> None:
+        self._messages = []
+def respond_to_message(history, message, slug):
+    vectorstore = Vectorstore()
+    chat_history = PassedInChatHistory()
+    for item in history:
+        if item['speaker'] == 'human':
+            chat_history.add_user_message(item)
+        else:
+            chat_history.add_ai_message(item)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, chat_memory=chat_history)
+
+    llm = ChatOpenAI(openai_api_key=config('OPENAI_API_KEY'), temperature=0)
+    # chain = load_qa_with_sources_chain(llm, chain_type="stuff",
+    #                                    retriever=vectorstore.get_collection(search_engine).as_retriever())
+    chain = ConversationalRetrievalChain.from_llm(llm, vectorstore.get_collection(slug).as_retriever(),
+                                                  memory=memory)
+    response = chain.run(question=message)
+    comparison = vectorstore.similarity_search(message, slug, 1)
+    logger.info(comparison)
+    logger.info(response)
+    return response
---
+++ decisions/service.py
@@ -0,0 +1,56 @@
+from typing import List
+
+from content.snippets import create_snippet
+from decisions.models import SearchEngineSearchEngine, SearchableLink
+from embeddings.vectorstore import Vectorstore
+
+
+def pick_search_engines_to_use(slug, query):
+    vectorstore = Vectorstore()
+    search_engine_search_engine = SearchEngineSearchEngine.objects.get(slug=slug)
+    number_of_results = search_engine_search_engine.search_engines.count()
+    search_engines_to_search = vectorstore.similarity_search(query, slug,
+                                                             k=number_of_results if number_of_results < 3 else 3)
+    return search_engines_to_search
+
+
+def compile_search_results(search_engines_to_search: List, query):
+    results = []
+    vectorstore = Vectorstore()
+
+    for search_engine in search_engines_to_search:
+
+        number_of_search_results = search_engine.searchable_links.count()
+
+        intermediate_results = vectorstore.similarity_search(query, search_engine.slug,
+                                                        k=number_of_search_results if number_of_search_results < 4 else 4)
+        for result in intermediate_results:
+            if result.metadata.get('search_engine'):
+                continue
+            section_id = result.metadata.get('section')
+            # fulltext_id = result['metadata']['fulltext']
+
+            link_id = result.metadata['link']
+            searchable_link = SearchableLink.objects.filter(id=link_id).first()
+            if searchable_link is None:
+                continue
+            if section_id:
+                snippet = create_snippet(section_id)
+            else:
+                snippet = None
+            if searchable_link and searchable_link.title:
+                title = searchable_link.title
+            else:
+                title = "Missing Title"
+
+            if searchable_link and searchable_link.description:
+                description = searchable_link.description
+            else:
+                description = None
+            results.append({
+                "title": title,
+                "link": searchable_link.url.url,
+                "snippet": snippet,
+                "description": description
+            })
+    return results
