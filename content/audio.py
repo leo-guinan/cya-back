3ff606c9196e8c166be3041424e93ac2dec7faff
leo-guinan <leo@copy.ai>
2023-07-08 16:06:11 -0400

    deleting files when done
    Former-commit-id: addde5425f623c5a120630b202514c57e612f8e2

--- content/audio.py
+++ content/audio.py
@@ -1,5 +1,8 @@
-from content.transcriber import Transcriber
+import logging
+import os
 
+from content.transcriber import Transcriber
+logger = logging.getLogger(__name__)
 
 def extract_audio(video):
     # extract audio from video
@@ -8,5 +11,12 @@ def extract_audio(video):
 
 def transcribe_audio(audio_file) -> list:
     """Transcribe audio file and return list of chunks"""
-    transcriber = Transcriber()
-    return transcriber.transcribe(audio_file)
+    chunks = []
+    try:
+        transcriber = Transcriber()
+        chunks = transcriber.transcribe(audio_file)
+    except Exception as e:
+        logger.error(e)
+    finally:
+        os.remove(audio_file)
+        return chunks
