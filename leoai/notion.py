1709609a1b1606a5826c809cde43a9143e4c774c
leo-guinan <leo@copy.ai>
2023-07-11 11:29:02 -0400

    updating notion bit to update transcript instead of page body

--- leoai/notion.py
+++ leoai/notion.py
@@ -5,24 +5,28 @@ class Notion():
     def __init__(self):
         self.notion = Client(auth=config('NOTION_API_KEY'))
 
-    def add_chunks_to_page(self, page, chunks):
+    def add_chunks_to_page_property(self, page, chunks):
+        children = []
         for chunk in chunks:
-            self.notion.blocks.children.append(page, children=[
-                {
-                    "object": "block",
-                    "type": "paragraph",
-                    "paragraph": {
-                        "rich_text": [
-                            {
-                                "type": "text",
-                                "text": {
-                                    "content": chunk
-                                }
-                            }
-                        ]
-                    }
-                }
-            ])
+            children.append({
+                "type": "text",
+                "text": {
+                    "content": chunk,
+                    "link": None
+                },
+                "annotations": {
+                    "bold": False,
+                    "italic": False,
+                    "strikethrough": False,
+                    "underline": False,
+                    "code": False,
+                    "color": "default"
+                },
+                "plain_text": chunk,
+                "href": None
+            })
+
+        self.notion.pages.update(page, properties={'Transcript': children})
 
 
 
--- leoai/urls.py
+++ leoai/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from leoai.views import search, add_to_collection, get_collection, add_fact, get_facts, youtube_to_notion
+from leoai.views import search, add_to_collection, get_collection, add_fact, get_facts, youtube_to_notion, blog_webhook, \
+    thread_webhook
 
 urlpatterns = [
     path('search/', search),
@@ -9,4 +10,6 @@ urlpatterns = [
     path('get-collection/', get_collection),
     path('get-facts/', get_facts),
     path('youtube/', youtube_to_notion),
+    path('webhook/thread/', thread_webhook),
+    path('webhook/blog/', blog_webhook),
 ]
