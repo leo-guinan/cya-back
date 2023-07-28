f7741cfe1b4de03710bfab0c34a8780786b8d2b9
leo-guinan <leo@copy.ai>
2023-07-28 09:40:34 -0400

    rearranging code

--- backend/asgi.py
+++ backend/asgi.py
@@ -14,11 +14,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from coach.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django_asgi_app = get_asgi_application()
+
+from coach.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
