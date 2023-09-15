91bf247ae70ba2c1db35c72f52b8b71b1cce5b36
leo-guinan <leo@copy.ai>
2023-09-15 15:08:09 -0400

    working experiments

--- experiments/service.py
+++ experiments/service.py
@@ -1,13 +1,19 @@
+import os
+import uuid
+
 import boto3
+import pandas as pd
 import pinecone
 from decouple import config
 from langchain.agents import create_csv_agent
 from langchain.agents.agent_types import AgentType
+from langchain.chains.query_constructor.schema import AttributeInfo
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredExcelLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.prompts import ChatPromptTemplate
+from langchain.retrievers import SelfQueryRetriever
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.vectorstores import Pinecone
@@ -18,22 +24,44 @@ from experiments.models import UploadedFile
 # initialize pinecone
 
 
-def version_one(message):
-    agent = create_csv_agent(
-        OpenAI(temperature=0, openai_api_key=config("OPENAI_API_KEY")),
-        "FinancialSample.csv",
-        verbose=True,
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    )
-    response = agent.run(message)
-    return response
+def version_one(file_path, message):
+    xls = pd.ExcelFile(file_path)
+    # Loop over each sheet
+    for sheet in xls.sheet_names:
+        # Read the sheet to pandas dataframe
+        df = pd.read_excel(xls, sheet)
+
+        # Write the dataframe to csv
+        csv_file_name = f"{sheet}.csv"
+        df.to_csv(csv_file_name, index=False)
+        print(f"{csv_file_name} has been created.")
+
+    # Get all the csv files in the directory
+    csv_files = [f for f in os.listdir(os.curdir) if f.endswith('.csv')]
+    responses = []
+    for csv_file in csv_files:
+
+        agent = create_csv_agent(
+            OpenAI(temperature=0, openai_api_key=config("OPENAI_API_KEY")),
+            csv_file,
+            verbose=True,
+            agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        )
+        response = agent.run(message)
+        responses.append(response)
+        os.remove(csv_file)
+    return "\n".join(responses)
 
 
 def version_two(message):
     embeddings = OpenAIEmbeddings(openai_api_key=config("OPENAI_API_KEY"))
 
-    index = pinecone.Index("langchain-demo")
-    vectorstore = Pinecone(index, embeddings.embed_query, "text")
+    index = pinecone.Index(config("BIPC_PINECONE_INDEX_NAME"))
+
+    vectorstore = Pinecone(index,
+                           embeddings.embed_query,
+                           "text",
+                           namespace="excel-experiment")
     retriever = vectorstore.as_retriever()
     template = """Answer the question based only on the following context:
     {context}
@@ -42,7 +70,7 @@ def version_two(message):
     """
     prompt = ChatPromptTemplate.from_template(template)
 
-    model = ChatOpenAI()
+    model = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"), model_name="gpt-4-32k")
     chain = (
             {"context": retriever, "question": RunnablePassthrough()}
             | prompt
@@ -52,36 +80,90 @@ def version_two(message):
     return chain.invoke(message)
 
 
-def version_three(message):
-    return "version 3"
+def version_three(message, s3_bucket, s3_key):
+    embeddings = OpenAIEmbeddings(openai_api_key=config("OPENAI_API_KEY"))
+    index = pinecone.Index(config("BIPC_PINECONE_INDEX_NAME"))
+
+    vectorstore = Pinecone(index,
+                           embeddings.embed_query,
+                           "text",
+                           namespace="excel-experiment")
+    metadata_field_info = [
+        AttributeInfo(
+            name="s3_bucket",
+            description="The s3 bucket where the file is stored",
+            type="string or list[string]",
+        ),
+        AttributeInfo(
+            name="s3_key",
+            description="The s3 key where the file is stored",
+            type="integer",
+        ),
+        AttributeInfo(
+            name="description",
+            description="What this file contains",
+            type="string",
+        ),
+
+    ]
+    document_content_description = "a spreadsheet containing financial data for an account"
+    llm = OpenAI(temperature=0, openai_api_key=config("OPENAI_API_KEY"))
+    retriever = SelfQueryRetriever.from_llm(
+        llm, vectorstore, document_content_description, metadata_field_info, verbose=True
+    )
+    template = f"""Answer the question based only on the following context:
+        {{context}}
+        
+        The location of the file is: {s3_bucket}/{s3_key}
+
+        Question: {{question}}
+        """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    model = ChatOpenAI(openai_api_key=config("OPENAI_API_KEY"), model_name="gpt-4-32k")
+    chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | prompt
+            | model
+            | StrOutputParser()
+    )
+    return chain.invoke(message)
 
 
-def load_xlsx_to_pinecone(s3_bucket, s3_key, index_name):
+def load_xlsx_to_pinecone(s3_bucket, s3_key):
     embeddings = OpenAIEmbeddings(openai_api_key=config("OPENAI_API_KEY"))
 
     # download file from s3 bucket
-    # s3 = boto3.client('s3')
-    # with open('data.xlsx', 'wb') as f:
-    #     s3.download_fileobj(s3_bucket, s3_key, f)
-    loader = UnstructuredExcelLoader('Financial Sample.xlsx', mode="elements")
+    s3 = boto3.client('s3')
+    with open('data.xlsx', 'wb') as f:
+        s3.download_fileobj(s3_bucket, s3_key, f)
+    loader = UnstructuredExcelLoader('data.xlsx', mode="elements")
     docs = loader.load_and_split()
     pinecone.init(
         api_key=config("PINECONE_API_KEY"),  # find at app.pinecone.io
         environment=config("PINECONE_ENV"),  # next to api key in console
     )
 
-    index_name = "excel-index"
+    index = pinecone.Index(config("BIPC_PINECONE_INDEX_NAME"))
 
-    # First, check if our index already exists. If it doesn't, we create it
-    if index_name not in pinecone.list_indexes():
-        # we create a new index
-        pinecone.create_index(
-            name=index_name,
-            metric='cosine',
-            dimension=1536
-        )
-    # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
-    Pinecone.from_documents(docs, embeddings, index_name=index_name)
+
+    vectorstore = Pinecone(index,
+                           embeddings.embed_query,
+                           "text",
+                           namespace="excel-experiment")
+    texts = []
+    metadatas = []
+    ids = []
+    for doc in docs:
+        texts.append(doc.page_content)
+        metadatas.append({
+            "s3_bucket": s3_bucket,
+            "s3_key": s3_key,
+        })
+        ids.append(str(uuid.uuid4()))
+
+    vectorstore.add_texts(texts, metadatas=metadatas, ids=ids, namespace="excel-experiment")
+    os.remove('data.xlsx')
 
 
 def s3_upload(file):
@@ -92,8 +174,26 @@ def s3_upload(file):
 
     try:
         s3_client.upload_fileobj(file, config('EXPERIMENT_AWS_BUCKET_NAME'), file.name)
+        print("File uploaded successfully")
         uploaded = UploadedFile(bucket=config('EXPERIMENT_AWS_BUCKET_NAME'), key=file.name)
         uploaded.save()
+        print("file saved to db")
+        load_xlsx_to_pinecone(config('EXPERIMENT_AWS_BUCKET_NAME'), file.name)
+        print("file loaded to pinecone")
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def s3_download(file):
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=config('AWS_ACCESS_KEY'),
+                             aws_secret_access_key=config('AWS_SECRET_KEY')
+                             )
+
+
+    try:
+        s3_client.download_file(config('EXPERIMENT_AWS_BUCKET_NAME'), file, file)
         return True
     except Exception as e:
         print(e)
--- experiments/views.py
+++ experiments/views.py
@@ -1,11 +1,15 @@
 import json
 
+import pinecone
+from decouple import config
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, renderer_classes, permission_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework_api_key.permissions import HasAPIKey
-from experiments.service import version_one, version_two, version_three, s3_upload
+
+from experiments.models import UploadedFile
+from experiments.service import version_one, version_two, version_three, s3_upload, s3_download
 
 
 # Create your views here.
@@ -16,11 +20,17 @@ from experiments.service import version_one, version_two, version_three, s3_uplo
 @renderer_classes((JSONRenderer,))
 @permission_classes((HasAPIKey,))
 def run_requests(request):
+    pinecone.init(
+        api_key=config("PINECONE_API_KEY"),  # find at app.pinecone.io
+        environment=config("PINECONE_ENV"),  # next to api key in console
+    )
     body = json.loads(request.body)
     message = body['message']
-    v1 = version_one(message)
+    upload_file = UploadedFile.objects.latest('created_at')
+    s3_download(upload_file.key)
+    v1 = version_one(upload_file.key, message)
     v2 = version_two(message)
-    v3 = version_three(message)
+    v3 = version_three(message, upload_file.bucket, upload_file.key)
 
     return Response({'version_one': v1, 'version_two': v2, 'version_three': v3})
 
@@ -33,6 +43,8 @@ def upload_file(request):
 
     # Save file in S3.
     response = s3_upload(file)
+
+
 
     if response:
         return Response({"message": "Successful upload"})
