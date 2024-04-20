d7059b57d320a4412175fb00190d9884ad78d45f
Leo Guinan <leo.guinan@gmail.com>
2024-04-20 15:20:55 -0400

    prelo setup

--- prelo/urls.py
+++ prelo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 
-from prelo.views import ask, details
+from prelo.views import ask, details, check_for_answer
 
 urlpatterns = [
     path('ask/', ask),
     path('details/', details),
+    path('check/', check_for_answer),
 
 ]
--- prelo/views.py
+++ prelo/views.py
@@ -1,21 +1,47 @@
 import json
+import uuid
 
-from django.shortcuts import render
+from decouple import config
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import permission_classes, renderer_classes, api_view
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from rest_framework_api_key.permissions import HasAPIKey
+from submind.models import Goal
+from submind.tasks import think
 
 
 # Create your views here.
-@csrf_exempt
 @api_view(('POST',))
 @renderer_classes((JSONRenderer,))
-@permission_classes([])
+@permission_classes((HasAPIKey,))
 def ask(request):
     body = json.loads(request.body)
-    print(body)
-    return Response({'message': 'Hello, World!'})
+    question = body["question"]
+    fast_mode = body.get("fast_mode", False)
+    client_id = body["prelo_client_id"]
+    submind_id = config("PRELO_SUBMIND_ID")
+
+    goal = Goal.objects.create(content=question, submind_id=submind_id, fast=fast_mode, client_id=client_id,
+                               uuid=str(uuid.uuid4()))
+
+    think.delay(goal.id)
+
+    return Response({'request_id': goal.id})
+
+
+@api_view(('POST',))
+@renderer_classes((JSONRenderer,))
+@permission_classes((HasAPIKey,))
+def check_for_answer(request):
+    body = json.loads(request.body)
+    goal_id = body["request_id"]
+    goal = Goal.objects.get(id=goal_id)
+    if goal.completed and goal.results:
+        return Response({'answer': goal.results})
+    else:
+        return Response({'message': 'Still thinking...'})
+
 
 @csrf_exempt
 @api_view(('POST',))
